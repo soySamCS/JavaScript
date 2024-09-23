b287fcb352d3f107bbc2cf6ab549afc8b36746e8
Sam <163355895+soySamCS@users.noreply.github.com>
2024-09-23 12:59:59 -0600

    Aprendimos objetos y funciones como tipos de datos

--- 01-Fundamentos/Tipos-de-datos.js
+++ 01-Fundamentos/Tipos-de-datos.js
@@ -23,11 +23,19 @@
   Es el valor que se asigna a una varible que ha sido declara pero no inicializada.
 
 
-  2. Tipo de Datos Complejos.
+  2. Tipo de Datos Complejos o de Objetos.
 
   * Array = Arreglo
 
   Es un conjunto de datos que los almacena en una lista odenada de elementos. Los datos dentro de los arreglos se escrbiben entre corchetes [] y separados por coma ,
+
+  *Object
+
+  Es un conjunto de múltiples datos en pares 'clave' 'valor'. Se escreben entre llaves {} y separados por comas ,
+
+  * Functions o Funciones
+
+  Es un tipo de datos que puede ser "invocado". Se declaran con la palabra reservada "function" y el código que ejecuta entre llave ["código"]
 
 */
 
@@ -67,10 +75,42 @@ console.log(x); // Valor que el usuario introduzca.
 
 /* Array */
 
-let misVidejuegosFavoritos = ["Halo","Valorant","Apex"];
+let misVideojuegosFavoritos = ["Halo","Valorant","Apex"];
 let misNumerosSuerte = [2, 9, 7];
 let otroArray = ["Noble 6", 8, true]
 
-console.log(misVidejuegosFavoritos);
+// Accedemos a los datos de una clave dentro de un objeto.
+// Con la notación de []
+console.log(misVideojuegosFavoritos);
 console.log(misNumerosSuerte);
 console.log(otroArray);
+
+/* Objeto */
+
+let persona = {
+  // clave : valor,
+  nombre : "Samuel",
+  edad : 23,
+  colorCabello : "Castaño - obscuro",
+  colorOjos : "Cafes",
+  altura : 1.77,
+  peso : "55 kg",
+  vivo : true,
+  novia : false,
+}
+
+console.log(persona);
+// Accedemos a los datos de una clave dentro de un objeto.
+// Con la notación de .
+console.log(persona.nombre);
+console.log(persona.novia);
+
+/* Function */
+
+let mensajito = function () {
+  // Pasamos el código que ejecuta la función
+  console.log("Este es un mensaje lanzado por una función");
+}
+
+//Invocamos la función
+mensajito();
